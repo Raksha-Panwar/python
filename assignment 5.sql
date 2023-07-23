fe2ac7163bbe9c62170ee1d356daefd7e40b0fa9
Raksha-Panwar <135188990+Raksha-Panwar@users.noreply.github.com>
2023-07-23 10:17:07 +0530

    sql script assignment 5

---
+++ assignment 5.sql
@@ -0,0 +1,9 @@
+-- create database assignments
+
+-- use assignments
+
+-- create table store_sales (month varchar(7),prod_name varchar(35),sales int)
+
+-- insert into store_sales (month,prod_name ,sales) values ('Jan','Fruits',45000),('Jan','vegetables',67000),('Jan','dairy',55000),('Feb','Fruits',42000),('Feb','Vegetables',32000),('Feb','dairy',52000),('March','Fruits',61000),('March','Vegetables',43000),('March','dairy',92000)
+
+select * from store_sales
